830e46b84d7cfc9d0b08885a447109d9b3cae694
tasha-olivia <tashaolivia9@gmail.com>
2022-05-19 10:50:01 +0100

    OOP project
    A system that manages all interactions in a 'njangi'

---
+++ Njangi_management system/src/njangi_management/system/Admin_login.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package njangi_management.system;
+
+/**
+ *
+ * @author mike
+ */
+public class Admin_login {
+    
+}
---
+++ Njangi_management system/src/njangi_management/system/Njangi_managementSystem.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package njangi_management.system;
+
+/**
+ *
+ * @author mike
+ */
+public class Njangi_managementSystem {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
